a82d4806a11d25b8d908f970a79f168a2f247c25
Marcelo de Carvalho Lopes <marcelolcarvalho@gmail.com>
2026-03-30 19:46:15 -0300

    exercicio-03

---
+++ modulo-01/exercicio-03.js
@@ -0,0 +1,30 @@
+//3 - Utilize switch/case para escrever um código que receba o nome de uma peça de xadrez e retorne os movimentos que ela pode fazer
+
+let peca = "rainha";
+
+switch (peca) {
+  case "peao":
+    console.log("Anda 1 casa para frente");
+    break;
+  case "torre":
+    console.log("Linhas retas: Horizontal e Vertical");
+    break;
+  case "cavalo":
+    console.log("Em formato de L: 2 casas em uma direção + 1 para o lado");
+    break;
+  case "bispo":
+    console.log("Diagonal ↗ ↘ ↖ ↙, quantas casas quiser ");
+    break;
+  case "rainha":
+    console.log(
+      "Combina torre + bispo, Horizontal,Vertical,Diagonal, Quantas casas quiser",
+    );
+    break;
+  case "rei":
+    console.log("1 casa em qualquer direção");
+    break;
+
+  default:
+    console.log("Peça não encontrada");
+    break;
+}
